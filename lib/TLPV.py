e94945b3df87403b44d76528971496885e34db92
rodrigo <rodrigo@14dfde1f-e7c9-46a2-be92-ccb7d42526f0>
2006-11-20 17:01:44 +0000

    Added cast in pfl attribute in strip creation. Fixed bug in TWR-AppConv.fir

--- lib/TLPV.py
+++ lib/TLPV.py
@@ -198,7 +198,7 @@ class TLPV:
             cfd.origin          = a.adep
             cfd.destination     = a.ades
             cfd.fl              = "%d"%a.rfl
-            cfd.cfl             = a.pfl
+            cfd.cfl             = "%d"%a.pfl
             cfd.cssr            = "%04o"%a.squawk
             cfd.route           = route
             cfd.rules           = ""
