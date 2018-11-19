03d54ed20412950c1854c502176c60f6ca22f5c0
Juan Toledo <toledo+github@lazaro.es>
2018-11-19 10:07:09 +0100

    LNAV.py Changed f. for a. Aircraft is constant, flight is for each leg, more appropriate for flight plan management calculations

--- lib/LNAV.py
+++ lib/LNAV.py
@@ -17,7 +17,10 @@
 # along with CrujiSim; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-"""Outputs desired course track for an LNAV mode"""
+"""
+Outputs desired course track for an LNAV mode
+All functions are passed the aircraft instance a
+"""
 
 import logging
 
@@ -53,196 +56,195 @@ ORBIT = "ORBIT"
 # etc. references, and wind_drift corrections should probably be done outside of this
 
 
-def fpr(f, wind_drift):
+def fpr(a, wind_drift):
     """Follow flight plan route"""
-    f.vect = f.get_bearing_to_next()
+    a.vect = a.get_bearing_to_next()
     # wind_drift correction
-    return f.vect[1] - wind_drift
+    return a.vect[1] - wind_drift
 
 
-def hdg(f):
+def hdg(a):
     """Fly heading"""
-    tgt_hdg = f.to_do_aux[0]
+    tgt_hdg = a.to_do_aux[0]
 
-    f.vect = rp((2.0 * f.ground_spd, tgt_hdg))
-    rel_a = relative_angle(tgt_hdg, f.hdg)
+    a.vect = rp((2.0 * a.ground_spd, tgt_hdg))
+    rel_a = relative_angle(tgt_hdg, a.hdg)
 
     # When the turn direction is forced, we provide an intermediate target heading
     # which is 90 degrees on the direction we have been asked to turn
-    if f.to_do_aux[1] == 'DCHA':
+    if a.to_do_aux[1] == 'DCHA':
         if rel_a > 0.:  # El rumbo está a su izquierda
-            return (f.hdg + 90.) % 360.
+            return (a.hdg + 90.) % 360.
         else:  # Está a su derecha o ya está en rumbo
-            return f.to_do_aux[0]
+            return a.to_do_aux[0]
 
-    elif f.to_do_aux[1] == 'IZDA':
+    elif a.to_do_aux[1] == 'IZDA':
         if rel_a < 0.:  # El rumbo está a su derecha
-            return (f.hdg - 90.) % 360.
+            return (a.hdg - 90.) % 360.
         else:  # Está a su izquierda o ya está en rumbo
-            return f.to_do_aux[0]
+            return a.to_do_aux[0]
 
     # No forced turn
-    return f.to_do_aux[0]
+    return a.to_do_aux[0]
 
 
-def orbit(f):
+def orbit(a):
     """Orbit on present position"""
-    if f.to_do_aux[0] == 'DCHA':
-        return (f.hdg + 90.0) % 360.0
+    if a.to_do_aux[0] == 'DCHA':
+        return (a.hdg + 90.0) % 360.0
     else:
-        return (f.hdg - 90.0) % 360.0
+        return (a.hdg - 90.0) % 360.0
 
 
-def hold(f, wind_drift, t):
+def hold(a, wind_drift, t):
     """Fly published holding pattern"""
-    if not f.to_do_aux[4] and f.to_do_aux[0] in f.route:
+    if not a.to_do_aux[4] and a.to_do_aux[0] in a.route:
         # Aún no ha llegado a la espera, sigue volando en ruta
         # Punto al que se dirige con corrección de wind_drift
-        pos = f.route[0].pos()
-        f.vect = rp(r(pos, f.pos))
+        pos = a.route[0].pos()
+        a.vect = rp(r(pos, a.pos))
         # Correción de wind_drift
-        return f.vect[1] - wind_drift
+        return a.vect[1] - wind_drift
 
     # Está dentro de la espera, entramos bucle de la espera
-    f.to_do_aux[4] = True
+    a.to_do_aux[4] = True
 
     # El fijo principal debe estar en la ruta. Si no está se pone
-    if not f.to_do_aux[0] in f.route:
-        f.route.insert(0, WayPoint(f.to_do_aux[0]))
+    if not a.to_do_aux[0] in a.route:
+        a.route.insert(0, WayPoint(a.to_do_aux[0]))
 
     # Con esta operación no nos borra el punto de la espera
-    f.vect = rp((2.0 * f.ground_spd, f.track))
+    a.vect = rp((2.0 * a.ground_spd, a.track))
 
-    if len(f.to_do_aux) == 6:  # Vamos a definir el rumbo objetivo, añadiéndolo al final
-        inbd_hdg = (f.to_do_aux[1] - wind_drift) % 360.0
+    if len(a.to_do_aux) == 6:  # Vamos a definir el rumbo objetivo, añadiéndolo al final
+        inbd_hdg = (a.to_do_aux[1] - wind_drift) % 360.0
 
-        aux = relative_angle(inbd_hdg, f.hdg)
+        aux = relative_angle(inbd_hdg, a.hdg)
         if aux > -60.0 and aux < 120.0:  # Entrada directa
             # Rumbo de alejamiento (con corrección de wind_drift)
-            target_hdg = (f.to_do_aux[1] + 180.0 - wind_drift) % 360.
+            target_hdg = (a.to_do_aux[1] + 180.0 - wind_drift) % 360.
         else:
             # Rumbo de alejamiento (con corrección de wind_drift)
             target_hdg = - \
-                ((f.to_do_aux[1] + 180.0 - 30.0 *
-                  f.to_do_aux[5] - wind_drift) % 360.)
-        f.to_do_aux.append(target_hdg)
+                ((a.to_do_aux[1] + 180.0 - 30.0 *
+                  a.to_do_aux[5] - wind_drift) % 360.)
+        a.to_do_aux.append(target_hdg)
 
-    target_hdg = f.to_do_aux[6]
+    target_hdg = a.to_do_aux[6]
     if target_hdg < 0.0:
         target_hdg = -target_hdg
     else:
-        if abs(target_hdg - f.hdg) > 60.0:
-            target_hdg = (f.hdg + 90.0 * f.to_do_aux[5]) % 360.0
+        if abs(target_hdg - a.hdg) > 60.0:
+            target_hdg = (a.hdg + 90.0 * a.to_do_aux[5]) % 360.0
 
     # Está en el viraje hacia el tramo de alejamiento
-    if f.to_do_aux[3] == 0.0 or f.to_do_aux[3] == -10.0:
-        if abs(target_hdg - f.hdg) < 1.:  # Ha terminado el viraje
-            if f.to_do_aux[3] == -10.0:
-                f.to_do_aux[4] = False
-                f.to_do_aux[3] = 0.0
-                f.to_do_aux.pop(6)
+    if a.to_do_aux[3] == 0.0 or a.to_do_aux[3] == -10.0:
+        if abs(target_hdg - a.hdg) < 1.:  # Ha terminado el viraje
+            if a.to_do_aux[3] == -10.0:
+                a.to_do_aux[4] = False
+                a.to_do_aux[3] = 0.0
+                a.to_do_aux.pop(6)
             else:
-                f.to_do_aux[3] = t
+                a.to_do_aux[3] = t
 
     # Comprobar tiempo que lleva en alejamiento y al terminar entra en
     # acercamiento
-    elif (t > f.to_do_aux[2] + f.to_do_aux[3]):
-        f.to_do_aux[3] = -10.0
-        f.to_do_aux[6] = f.to_do_aux[1]
+    elif (t > a.to_do_aux[2] + a.to_do_aux[3]):
+        a.to_do_aux[3] = -10.0
+        a.to_do_aux[6] = a.to_do_aux[1]
 
     return target_hdg
 
 
-def hdgfix(f, wind_drift):
+def hdgfix(a, wind_drift):
     """Fly a heading after passing a fix"""
-    if f.to_do_aux[0] in f.route:
+    if a.to_do_aux[0] in a.route:
         # Punto al que se dirige con corrección de wind_drift
-        f.vect = rp(r(f.route[0].pos(), f.pos))
+        a.vect = rp(r(a.route[0].pos(), a.pos))
         # Correción de wind_drift
-        return f.vect[1] - wind_drift
+        return a.vect[1] - wind_drift
     else:
-        f.vect = rp((2.0 * f.ground_spd, f.track))
-        return f.to_do_aux[1]
+        a.vect = rp((2.0 * a.ground_spd, a.track))
+        return a.to_do_aux[1]
 
 
-def intercept_radial(f, wind_drift):
+def intercept_radial(a, wind_drift):
     """Intercept and follow radial from a point"""
-    (rx, ry) = r(WayPoint(f.to_do_aux[0]).pos(), f.pos)
+    (rx, ry) = r(WayPoint(a.to_do_aux[0]).pos(), a.pos)
     current_radial = rp((rx, ry))[1]
-    tgt_radial = f.to_do_aux[1]
+    tgt_radial = a.to_do_aux[1]
 
     aux = relative_angle(current_radial, tgt_radial)
-    (rdlx, rdly) = pr((1.0, f.to_do_aux[1]))
+    (rdlx, rdly) = pr((1.0, a.to_do_aux[1]))
     dist_perp = abs(rx * rdly - ry * rdlx)
 
     if dist_perp < 0.1:  # Consideramos que está en el radial
-        f.vect = rp((2.0 * f.ground_spd, f.track))
-        f.tgt_hdg = f.to_do_aux[1]
-        return f.to_do_aux[1] - wind_drift
+        a.vect = rp((2.0 * a.ground_spd, a.track))
+        a.tgt_hdg = a.to_do_aux[1]
+        return a.to_do_aux[1] - wind_drift
 
     elif dist_perp < 0.8:
-        f.vect = rp((2.0 * f.ground_spd, f.track))
-        f.tgt_hdg = (f.to_do_aux[1] - 20.0 * sgn(aux)) % 360.0
-        return (f.to_do_aux[1] - wind_drift - 20.0 * sgn(aux)) % 360.0
+        a.vect = rp((2.0 * a.ground_spd, a.track))
+        a.tgt_hdg = (a.to_do_aux[1] - 20.0 * sgn(aux)) % 360.0
+        return (a.to_do_aux[1] - wind_drift - 20.0 * sgn(aux)) % 360.0
 
     else:
-        f.vect = rp((2.0 * f.ground_spd, f.track))
-        return (f.tgt_hdg - wind_drift) % 360.0
-        # return (f.to_do_aux[1] - wind_drift - 45.0 * sgn(ang_aux))%360.0
+        a.vect = rp((2.0 * a.ground_spd, a.track))
+        return (a.tgt_hdg - wind_drift) % 360.0
+        # return (a.to_do_aux[1] - wind_drift - 45.0 * sgn(ang_aux))%360.0
 
 
-def app(f, wind_drift):
+def app(a, wind_drift):
     try:
-        (puntos_alt, llz, puntos_map) = fir.iaps[f.iaf]
+        (puntos_alt, llz, puntos_map) = fir.iaps[a.iaf]
     except KeyError:
-        logging.warning(
-            "No IAF found when trying to set course for approach. Keeping current heading")
-        return f.hdg
+        logging.warning("No IAF found when trying to set course for approach. Keeping current heading")
+        return a.hdg
 
     [xy_llz, llz_radial, dist_ayuda, pdte_ayuda, alt_pista] = llz
-    if len(f.route) == 0:  # Es el primer acceso a app desde la espera. Se añaden los puntos
+    if len(a.route) == 0:  # Es el primer acceso a app desde la espera. Se añaden los puntos
         for [a, b, c, h] in puntos_alt:
-            f.route.append(WayPoint(b))
+            a.route.append(WayPoint(b))
         wp = WayPoint("_LLZ")
         wp._pos = xy_llz
-        f.route.append(wp)
+        a.route.append(wp)
 
     # An no está en el localizador, tocamos solamente la altitud y como plan
     # de vuelo
-    if len(f.route) > 1:
-        if f._map and '_LLZ' not in f.route:  # Ya estáfrustrando
+    if len(a.route) > 1:
+        if a._map and '_LLZ' not in a.route:  # Ya estáfrustrando
             for [a, b, c, h] in puntos_map:
-                if b == f.route[0].fix:
-                    f.cfl = h / 100.
-                    f.set_std_rate()
+                if b == a.route[0].fix:
+                    a.cfl = h / 100.
+                    a.set_std_rate()
                     break
         else:
             for [a, b, c, h] in puntos_alt:
-                if b == f.route[0].fix:
-                    f.cfl = h / 100.
+                if b == a.route[0].fix:
+                    a.cfl = h / 100.
                     break
         # Punto al que se dirige con corrección de wind_drift
-        f.pto = f.route[0].pos()
-        f.vect = rp(r(f.pto, f.pos))
+        a.pto = a.route[0].pos()
+        a.vect = rp(r(a.pto, a.pos))
         # Correción de wind_drift
-        return f.vect[1] - wind_drift
+        return a.vect[1] - wind_drift
 
-    if len(f.route) == 1:  # Interceptar localizador y senda de planeo
+    if len(a.route) == 1:  # Interceptar localizador y senda de planeo
         # Ya estáfrustrando hacia el último punto, asimilamos a plan de
         # vuelo normal
-        if f._map and '_LLZ' not in f.route:
-            f.lnav_mode = NAV
-            f.app_auth = False
-            f.app_fix = ''
-            f._map = False
+        if a._map and '_LLZ' not in a.route:
+            a.lnav_mode = NAV
+            a.app_auth = False
+            a.app_fix = ''
+            a._map = False
             # Punto al que se dirige con corrección de wind_drift
-            f.pto = f.route[0].pos()
-            f.vect = rp(r(f.pto, f.pos))
+            a.pto = a.route[0].pos()
+            a.vect = rp(r(a.pto, a.pos))
             # Correción de wind_drift
-            return f.vect[1] - wind_drift
+            return a.vect[1] - wind_drift
         else:
             # Coordenadas relativas a la radioayuda
-            (rx, ry) = r(xy_llz, f.pos)
+            (rx, ry) = r(xy_llz, a.pos)
             # Primero intersecta la senda de planeo cuando es inferior.
             # Solamente tocamos el rate de descenso
             dist_thr = rp((rx, ry))[0] - dist_ayuda
@@ -250,47 +252,47 @@ def app(f, wind_drift):
             if abs(dist_thr) < 0.50:  # Avión aterrizado
                 # En caso de estar 200 ft por encima, hace MAP o si ya ha
                 # pasado el LLZ
-                height_over_field = f.lvl * 100 - alt_pista
+                height_over_field = a.lvl * 100 - alt_pista
                 if height_over_field > 200. or abs(derrota - llz_radial) > 90.:
                     logging.debug("%s: height over field is %d at %.1fnm. Executing MAP"
-                                  % (f.callsign, height_over_field, dist_thr))
-                    f._map = True
+                                  % (a.callsign, height_over_field, dist_thr))
+                    a._map = True
 
-                if f._map:  # Procedimiento de frustrada asignado
-                    f.set_std_spd()
-                    f.set_std_rate()
-                    f.route = Route([WayPoint(p[1]) for p in puntos_map])
+                if a._map:  # Procedimiento de frustrada asignado
+                    a.set_std_spd()
+                    a.set_std_rate()
+                    a.route = Route([WayPoint(p[1]) for p in puntos_map])
                 else:
-                    logging.debug("%s: Landing" % f.callsign)
+                    logging.debug("%s: Landing" % a.callsign)
                     # Prevents cyclic imports
                     from .Aircraft import LANDED
                     return LANDED
 
             # TODO THIS IS ALL VNAV, REALLY ##########
-            if f.esta_en_llz:
+            if a.esta_en_llz:
                 # Interceptación de la senda de planeo. Se ajusta rate descenso y ajuste ias = perf.app_tas
                 # fl_gp = Flight level of the glidepath at the current point
                 fl_gp = (alt_pista * FEET_TO_LEVELS +
                          dist_thr * pdte_ayuda * NM_TO_LEVELS)
-                if fl_gp <= f.lvl:
+                if fl_gp <= a.lvl:
                     # If above the glidepath
-                    f.set_ias(f.perf.app_tas / (1.0 + 0.002 * f.lvl))
-                    f.cfl = alt_pista * FEET_TO_LEVELS
-                    rate = ((f.lvl - fl_gp) * 1 +  # Additional vertical speed to capture
-                            f.ground_spd * pdte_ayuda) * NM_TO_LEVELS  # Vert speed to descend with the glide
+                    a.set_ias(a.perf.app_tas / (1.0 + 0.002 * a.lvl))
+                    a.cfl = alt_pista * FEET_TO_LEVELS
+                    rate = ((a.lvl - fl_gp) * 1 +  # Additional vertical speed to capture
+                            a.ground_spd * pdte_ayuda) * NM_TO_LEVELS  # Vert speed to descend with the glide
                     # Unidades en ft/min
-                    achievable, max_rate = f.set_vertical_rate(rate)
+                    achievable, max_rate = a.set_vertical_rate(rate)
 
                     if not achievable:
-                        f.set_vertical_rate(-max_rate)
+                        a.set_vertical_rate(-max_rate)
                         rate *= LEVELS_PER_HOUR_TO_FPM
                         max_rate *= LEVELS_PER_HOUR_TO_FPM
                         logging.debug("%s: Distance to threshold: %.1fnm, altitude: %dft, glidepath altitude: %dft, gs: %dkts"
-                                      % (f.callsign, dist_thr, f.lvl * 100, fl_gp * 100., f.ground_spd))
+                                      % (a.callsign, dist_thr, a.lvl * 100, fl_gp * 100., a.ground_spd))
                         logging.debug("%s: Unable to set approach descent rate %dfpm. Max is %dfpm"
-                                      % (f.callsign, rate, max_rate))
+                                      % (a.callsign, rate, max_rate))
                 else:
-                    f.set_vertical_rate(0.001)
+                    a.set_vertical_rate(0.001)
                     # Ahora el movimiento en planta
 
             current_radial = rp((rx, ry))[1]
@@ -300,22 +302,22 @@ def app(f, wind_drift):
             dist_perp = abs(rx * rdly - ry * rdlx)
 
             if dist_perp < 0.1:  # Consideramos que estáen el radial
-                if abs(f.lvl - f.cfl) < 002.0:
-                    f.esta_en_llz = True
-                f.int_loc = False
-                f.vect = rp((2.0 * f.ground_spd, f.track))
+                if abs(a.lvl - a.cfl) < 002.0:
+                    a.esta_en_llz = True
+                a.int_loc = False
+                a.vect = rp((2.0 * a.ground_spd, a.track))
                 return llz_radial - wind_drift
 
             elif dist_perp < 0.8:
-                if abs(f.lvl - f.cfl) < 002.0:
-                    f.esta_en_llz = True
-                f.int_loc = False
-                f.vect = rp((2.0 * f.ground_spd, f.track))
+                if abs(a.lvl - a.cfl) < 002.0:
+                    a.esta_en_llz = True
+                a.int_loc = False
+                a.vect = rp((2.0 * a.ground_spd, a.track))
                 return (llz_radial - wind_drift - 20.0 * sgn(ang_aux)) % 360.0
 
             else:
-                if f.int_loc:
-                    current_radial = f.hdg
+                if a.int_loc:
+                    current_radial = a.hdg
                     if llz_radial < 180.0 and current_radial > llz_radial + 180.0:
                         current_radial = current_radial - 360.0
                     elif llz_radial > 180.0 and current_radial < llz_radial - 180.0:
@@ -325,19 +327,19 @@ def app(f, wind_drift):
                     # actual
                     ang_aux2 = llz_radial - current_radial
                     if ang_aux * ang_aux2 > 0.:
-                        return f.tgt_hdg - wind_drift
+                        return a.tgt_hdg - wind_drift
                     else:
-                        f.int_loc = False
-                        f.vect = rp((2.0 * f.ground_spd, f.track))
-                        f.tgt_hdg = llz_radial - 45.0 * sgn(ang_aux)
+                        a.int_loc = False
+                        a.vect = rp((2.0 * a.ground_spd, a.track))
+                        a.tgt_hdg = llz_radial - 45.0 * sgn(ang_aux)
                         return (llz_radial - wind_drift - 45.0 * sgn(ang_aux)) % 360.0
 
                 else:
-                    f.vect = rp((2.0 * f.ground_spd, f.track))
+                    a.vect = rp((2.0 * a.ground_spd, a.track))
                     return (llz_radial - wind_drift - 45.0 * sgn(ang_aux)) % 360.0
 
 
-def get_target_heading(f, wind_drift, t):
+def get_target_heading(a, wind_drift, t):
     """Return target heading for current flight phase"""
 
     tgt_hdg_functions = {
@@ -350,8 +352,8 @@ def get_target_heading(f, wind_drift, t):
         LOC_CAPTURE: (app, wind_drift)
     }
 
-    assert f.lnav_mode in tgt_hdg_functions, "Unknown lnav_mode %s" % f.lnav_mode
+    assert a.lnav_mode in tgt_hdg_functions, "Unknown lnav_mode %s" % a.lnav_mode
 
-    function = tgt_hdg_functions[f.lnav_mode][0]
-    args = [f] + list(tgt_hdg_functions[f.lnav_mode][1:])
+    function = tgt_hdg_functions[a.lnav_mode][0]
+    args = [a] + list(tgt_hdg_functions[a.lnav_mode][1:])
     return function(*args)
