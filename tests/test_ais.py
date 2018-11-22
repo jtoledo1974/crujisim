c760061a3be885a25bf283cdd68f6a0b16701993
Juan Toledo <toledo+github@lazaro.es>
2018-11-22 21:34:59 +0100

    TEST renaming fixtures so that the parametrized ais is the standard. Skip the tests that can only fail

--- tests/test_ais.py
+++ tests/test_ais.py
@@ -9,34 +9,48 @@ fir_files = basedir.glob('**/*.fir')
 
 
 @pytest.fixture
-def ais(fir_file):
-    AIS.init(str(fir_file))
+def ais_sample():
+    fir_file = next(basedir.glob('**/sample.fir'))
+    AIS.init(fir_file)
 
 
 @pytest.fixture(scope='module', params=fir_files)
-def ais_n(request):
+def ais(request):
     AIS.init(str(request.param))
+    return request.param
 
 
-def test_load_fir(fir_file):
-    AIS.init(str(fir_file))
-
+def test_load_fir(ais_sample):
     assert len(AIS.points) == 69
 
 
-def test_points(ais_n):
+def test_points(ais):
     for p in AIS.points.values():
         assert isinstance(p.designator, str)
         x, y = p.pos
         assert isinstance(x, float) and isinstance(y, float)
 
 
-def test_routes(ais_n):
+def test_routes(ais):
     for route in AIS.routes:
         assert len(route) > 0
         for p in route:
             assert p in (p.pos for p in AIS.points.values())
 
 
-def test_aerodromes(ais_n):
+def test_aerodromes(ais):
     assert len(AIS.aerodromes) > 0
+
+
+def test_sids_sids(ais):
+    for ad in AIS.aerodromes.values():
+        for rwy in ad.runwayDirections:
+            assert len(rwy.standardInstrumentDepartures) > 0
+
+
+def test_sids_stars(ais):
+    if ais.name in ('Ruta-Convencional.fir', 'Ruta-DGOx6.fir'):
+        pytest.skip("No STARS on these")
+    for ad in AIS.aerodromes.values():
+        for rwy in ad.runwayDirections:
+            assert len(rwy.standardInstrumentArrivals) > 0
